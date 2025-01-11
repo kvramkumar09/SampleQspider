0092f3e6b8c623f0fcc8234e823a934a300e49a5
kvram <kvram@Automation_JAVA>
2025-01-11 12:40:28 +0530

    changes committed

--- src/main/java/SimpleProj.java
+++ src/main/java/SimpleProj.java
@@ -2,9 +2,10 @@ import org.testng.annotations.Test;
 
 public class SimpleProj {
 
-@Test
+	@Test
 	public void sample() {
-	System.out.println("Git practice");
-	System.out.println("hello");
-}
+		System.out.println("Git practice");
+		System.out.println("hello");
+		
+	}
 }
